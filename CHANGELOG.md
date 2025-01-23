2b7900dad676b3ce84493a6953f91ca9c830109f
dermie83 <dermiemadsen@gmail.com>
2025-01-23 20:49:35 +0000

    changed nav-link page names

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Irish Beacon Watch Changelog
 
+## verson 0.2.0
+Removed login button from landing page
+Changed nav-links to application pages (Home, forecast, historical, about)
+
 ## verson 0.1.0
 Clone example boiler plate from nextjs official web page https://nextjs.org/learn/dashboard-app/getting-started
 Run pnpm i to install the project's packages.
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,12 +8,9 @@ import {
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  {name: 'Forecast',href: '/dashboard/forecast',icon: DocumentDuplicateIcon,},
+  { name: 'Historical', href: '/dashboard/historical', icon: UserGroupIcon },
+  { name: 'About', href: '/dashboard/about', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
