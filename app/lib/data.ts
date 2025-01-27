f8ae2c824485c34f5170d5f103a695e4f21254cf
dermie83 <dermiemadsen@gmail.com>
2025-01-27 21:05:50 +0000

    added lighthouse type

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import axios from 'axios';
 import {
   CustomerField,
   CustomersTableType,
@@ -8,6 +9,65 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+
+import { fetchWeatherApi } from 'openmeteo';
+
+	
+const params = {
+	"latitude":53.3,
+	"longitude": -7.0,
+	"hourly": "temperature_2m"
+};
+
+export async function fetchWeatherData() {
+	try {
+	const url =`https://api.open-meteo.com/v1/forecast`;
+	// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m
+	const responses = await fetchWeatherApi(url,params);
+
+	// Helper function to form time ranges
+	const range = (start: number, stop: number, step: number) =>
+		Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+	// Process first location. Add a for-loop for multiple locations or weather models
+	const response = responses[0];
+
+	// Attributes for timezone and location
+	const utcOffsetSeconds = response.utcOffsetSeconds();
+	const timezone = response.timezone();
+	const timezoneAbbreviation = response.timezoneAbbreviation();
+	const latitude = response.latitude();
+	const longitude = response.longitude();
+
+	const hourly = response.hourly()!;
+
+	// Note: The order of weather variables in the URL query and the indices below need to match!
+	const weatherData = {
+
+		hourly: {
+			time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+				(t) => new Date((t + utcOffsetSeconds) * 1000)
+			),
+			temperature2m: hourly.variables(0)!.valuesArray()!,
+		},
+
+	};
+
+	// `weatherData` now contains a simple structure with arrays for datetime and weather data
+	for (let i = 0; i < weatherData.hourly.time.length; i++) {
+		console.log(
+			weatherData.hourly.time[i].toISOString(),
+			weatherData.hourly.temperature2m[i]
+		);
+	}
+}
+	catch (error) {
+		console.error('API Error:', error);
+		throw new Error('Failed to fetch API data.');
+	}
+	
+}
+
 
 export async function fetchRevenue() {
   try {
